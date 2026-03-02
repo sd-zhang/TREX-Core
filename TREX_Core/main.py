15da26c155a2f46a5203cecc0d15fe40573ede8e
Steven <steven@trex-ai.ca>
2026-03-02 13:55:10 -0800

    good suggestions from peter
    minor changes

--- TREX_Core/main.py
+++ TREX_Core/main.py
@@ -4,13 +4,13 @@ if __name__ == '__main__':
     # file name must be config name. Case sensitive.
     # it is sometimes possible to resume a simulation in case it crashes in the middle
     # however due to the complexity this is experimental and cannot be relied upon
-    runner = Runner(config='mqtt_test', purge=True)
+    runner = Runner(config='citylearn_test', purge=True)
 
     # list of simulations to be performed.
     # in general, it is recommended to perform at base line and training at minimum
     simulations = {
-        # 'baseline'
-        'training'
+        'baseline'
+        # 'training'
         # 'validation'
     }
     runner.run_simulations(simulations)
